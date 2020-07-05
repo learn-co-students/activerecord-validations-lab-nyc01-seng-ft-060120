4eb9f44519c531188dde7d0e5127e663cb44c1ad
alexpac27 <alexpac27@gmail.com>
2020-07-05 13:47:25 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Author.create(name: "Steven King",phone_number: 2129999999)
+Author.create(name: "Agatha Christie",phone_number: 2129999999)
+Author.create(name: "Jeremy Hanson",phone_number: 2129999999)
+
+Post.create(title: "Post 1", content: "content 1", summary: "sum 1", category:"cat 1")
+Post.create(title: "Post 2", content: "content 2", summary: "sum 2", category:"cat 2")
+Post.create(title: "Post 3", content: "content 3", summary: "sum 3", category:"cat 3")
